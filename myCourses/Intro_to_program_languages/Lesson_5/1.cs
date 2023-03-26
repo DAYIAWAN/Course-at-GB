1bfe58c306d0042eb127d39129c98a6d51906502
DAYIAWAN <dayiawan@mail.ru>
2023-03-26 09:27:09 +0300

    Homework of 3 tasks

---
+++ myCourses/Intro_to_program_languages/Lesson_5/1.cs
@@ -0,0 +1,44 @@
+/*
+Задача 1:
+	Задайте массив, заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+		[345, 897, 568, 234]	->	2
+*/
+
+Console.WriteLine("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("В данном массиве: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел, {count} чётных:");
+
+
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ myCourses/Intro_to_program_languages/Lesson_5/3.cs
@@ -0,0 +1,47 @@
+/*
+Задача 3:
+	Задайте (с клавиатуры) массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+		[3, 7.4, 22.3, 2, 78]	->	76
+*/
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+        {
+            max = numbers[j];
+        }
+    if (numbers[j] < min)
+        {
+            min = numbers[j];
+        }
+}
+
+Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
