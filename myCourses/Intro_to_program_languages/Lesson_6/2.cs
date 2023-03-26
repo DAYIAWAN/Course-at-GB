06adddbd1721152cbd8b8621fc70ed4d4e37fc15
DAYIAWAN <dayiawan@mail.ru>
2023-03-26 23:45:49 +0300

    Homework of 2 tasks

--- myCourses/Intro_to_program_languages/Lesson_6/2.cs
+++ myCourses/Intro_to_program_languages/Lesson_6/2.cs
@@ -1,6 +1,23 @@
 /*
 Задача 2:
-	...
+	Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+	y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+		b1 = 2,
+		k1 = 5,
+		b2 = 4,
+		k2 = 9	->	(-0,5; -0,5)
 */
 
-...
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами: X - {x}, Y - {y}");
