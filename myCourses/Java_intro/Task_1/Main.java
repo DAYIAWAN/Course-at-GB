b418c695b4bf9cc69c41312b27e78422a1846465
DAYIAWAN <dayiawan@mail.ru>
2023-06-30 23:50:20 +0300

    Added new Lesson

---
+++ myCourses/Java_intro/Task_1/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+ public static void main(String[] args) {
+ RedBlackTree node = new RedBlackTree();
+
+ node.insert(10);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(20);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(30);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-40);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-50);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-60);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-700);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-800);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-900);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(1000);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(2000);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(3000);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-4001);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-5002);
+ node.inorder();
+ System.out.println("\n");
+ node.insert(-6003);
+ node.inorder();
+
+
+    }
+}
