99bc654b55535898e4f29c6dc36e0da538434270
DAYIAWAN <dayiawan@mail.ru>
2024-05-14 19:58:27 +0300

    HotFix

--- myCourses/PHP/Laravel/example-app/routes/web.php
+++ myCourses/PHP/Laravel/example-app/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/userform', 'FormProcessor@index');
+Route::post('/store_form', 'FormProcessor@store');
