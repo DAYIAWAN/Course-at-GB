65979834bee88886b1b497206276fa5618ae7999
DAYIAWAN <dayiawan@mail.ru>
2024-05-14 21:06:24 +0300

    HotFix

--- myCourses/PHP/Laravel/example-app/app/Http/Controllers/FormProcessor.php
+++ myCourses/PHP/Laravel/example-app/app/Http/Controllers/FormProcessor.php
@@ -10,4 +10,15 @@ class FormProcessor extends Controller
     {
         return view('form');
     }
+
+    public function store(Request $request)
+    {
+        // Здесь вы можете обработать данные формы, полученные из $request
+
+        // Например, вы можете просто вернуть все данные формы
+        // return $request->all();
+
+        // Или вы можете перенаправить пользователя обратно на форму
+        // return redirect('/userform');
+    }
 }
