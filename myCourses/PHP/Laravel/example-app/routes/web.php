f750dd0e944273de2c150c9c0ed0198190644fbf
DAYIAWAN <dayiawan@mail.ru>
2024-05-18 15:04:00 +0300

    HotFix

--- myCourses/PHP/Laravel/example-app/routes/web.php
+++ myCourses/PHP/Laravel/example-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormProcessor;
+use App\Models\Employee;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,27 @@ Route::get('/', function () {
 
 Route::get('/userform', [FormProcessor::class, 'index']);
 Route::post('/store_form', [FormProcessor::class, 'store']);
+
+Route::get('/test_database', function () {
+    $employee = new Employee;
+    $employee->surname = 'Иванов';
+    $employee->name = 'Иван';
+    $employee->patronymic = 'Иванович';
+    $employee->birth_date = '1950-01-01';
+    $employee->birth_place = 'Москва';
+    $employee->work_place = 'ООО "Рога и Копыта"';
+    $employee->phone = '+7 926 000-00-00';
+    $employee->email = 'info@example.com';
+    $employee->website = 'https://example.com';
+    $employee->save();
+
+    return "Employee created successfully!";
+});
+
+Route::get('/test_database_update', function () {
+    $employee = Employee::find(1);
+    $employee->surname = 'Петров';
+    $employee->save();
+
+    return "Employee updated successfully!";
+});
