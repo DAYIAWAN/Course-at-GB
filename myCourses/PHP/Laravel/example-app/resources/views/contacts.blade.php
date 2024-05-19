d13ba39ac412a3bebd8b9ed9017065af18983cb2
DAYIAWAN <dayiawan@mail.ru>
2024-05-19 09:55:01 +0300

    HotFix

--- myCourses/PHP/Laravel/example-app/resources/views/contacts.blade.php
+++ myCourses/PHP/Laravel/example-app/resources/views/contacts.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 
 @section('content')
-    // Add your markup here with the output of the passed data inside your route
-@stop
+    <h1>Контакты</h1>
+    <p>Адрес: {{ $address }}</p>
+    <p>Почтовый индекс: {{ $post_code }}</p>
+    @if (!empty($email))
+        <p>Электронная почта: {{ $email }}</p>
+    @else
+        <p>Адрес электронной почты не указан.</p>
+    @endif
+    <p>Телефон: {{ $phone }}</p>
+@endsection
--- myCourses/PHP/Laravel/example-app/resources/views/home.blade.php
+++ myCourses/PHP/Laravel/example-app/resources/views/home.blade.php
@@ -1,19 +1,12 @@
-<!doctype html>
-<html>
-<head>
-    @include('includes.head')
-</head>
-<body>
-<div class="container">
-    <header class="row">
-        @include('includes.header')
-    </header>
-    <div id="main" class="row">
-        @yield('content')
-    </div>
-    <footer class="row">
-        @include('includes.footer')
-    </footer>
-</div>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('content')
+    <h1>Добро пожаловать, {{ $name }}!</h1>
+    @if ($age > 18)
+        <p>Ваш возраст: {{ $age }}</p>
+    @else
+        <p>Указанный человек слишком молод.</p>
+    @endif
+    <p>Позиция: {{ $position }}</p>
+    <p>Адрес: {{ $address }}</p>
+@endsection
