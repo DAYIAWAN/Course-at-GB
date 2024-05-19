fc69f20ecf7fdae2c2516e894f0b059750407363
DAYIAWAN <dayiawan@mail.ru>
2024-05-19 22:57:48 +0300

    Edited file book_form.blade.php | Add error handling for incorrect validation

--- myCourses/PHP/Laravel/example-app/resources/views/book_form.blade.php
+++ myCourses/PHP/Laravel/example-app/resources/views/book_form.blade.php
@@ -7,18 +7,30 @@
 </head>
 <body>
     <h1>Добавить новую книгу</h1>
+
+    <!-- Отображение ошибок валидации -->
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="/store" method="POST">
         @csrf
         <label for="title">Название книги:</label>
-        <input type="text" id="title" name="title" required>
+        <input type="text" id="title" name="title" value="{{ old('title') }}" required>
 
         <label for="author">Имя автора:</label>
-        <input type="text" id="author" name="author" required>
+        <input type="text" id="author" name="author" value="{{ old('author') }}" required>
 
         <label for="genre">Жанр:</label>
         <select id="genre" name="genre">
-            <option value="fiction">Фантастика</option>
-            <option value="non-fiction">Нон-фикшн</option>
+            <option value="fiction" {{ old('genre') == 'fiction' ? 'selected' : '' }}>Фантастика</option>
+            <option value="non-fiction" {{ old('genre') == 'non-fiction' ? 'selected' : '' }}>Нон-фикшн</option>
             <!-- другие опции -->
         </select>
 
