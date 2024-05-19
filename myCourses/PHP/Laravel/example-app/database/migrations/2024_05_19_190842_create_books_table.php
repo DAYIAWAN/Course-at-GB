27f58331a47b7ac5d4dd355f1b4b8751c77f6d70
DAYIAWAN <dayiawan@mail.ru>
2024-05-19 22:14:59 +0300

    Created 2024_05_19_190842_create_books_table.php

--- myCourses/PHP/Laravel/example-app/database/migrations/2024_05_19_190842_create_books_table.php
+++ myCourses/PHP/Laravel/example-app/database/migrations/2024_05_19_190842_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('author', 100);
+            $table->string('genre');
             $table->timestamps();
         });
     }
