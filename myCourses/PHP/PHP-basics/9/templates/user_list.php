01332c4e0d19e28e41e12b2cfae89d7aa3e3313b
DAYIAWAN <dayiawan@mail.ru>
2024-08-21 20:48:48 +0300

    HotFix

--- myCourses/PHP/PHP-basics/9/templates/user_list.php
+++ myCourses/PHP/PHP-basics/9/templates/user_list.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'; ?>
+
 <table>
     <tr>
         <th>ID</th>
@@ -12,10 +13,33 @@
             <td><?php echo htmlspecialchars($user->name); ?></td>
             <td><?php echo htmlspecialchars($user->email); ?></td>
             <td>
-                <a href="user.php?id=<?php echo htmlspecialchars($user->id); ?>">Edit</a>
-                <a href="user.php?action=delete&id=<?php echo htmlspecialchars($user->id); ?>">Delete</a>
+                <?php if ($currentUser->role === 'admin'): ?>
+                    <a href="user.php?id=<?php echo htmlspecialchars($user->id); ?>">Edit</a>
+                    <a href="javascript:void(0);" class="delete-user" data-id="<?php echo htmlspecialchars($user->id); ?>">Delete</a>
+                <?php endif; ?>
             </td>
         </tr>
     <?php endforeach; ?>
 </table>
+
 <?php include 'footer.php'; ?>
+
+<script>
+    document.querySelectorAll('.delete-user').forEach(function(element) {
+        element.addEventListener('click', function() {
+            if (confirm('Are you sure you want to delete this user?')) {
+                var userId = this.getAttribute('data-id');
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', 'user.php?action=delete', true);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                        // Успешное удаление пользователя
+                        location.reload();
+                    }
+                };
+                xhr.send('id=' + userId);
+            }
+        });
+    });
+</script>
