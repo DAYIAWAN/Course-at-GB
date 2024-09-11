7cd637a3d5f5505ef17688adf286e453c9e51165
DAYIAWAN <dayiawan@mail.ru>
2024-09-11 16:31:48 +0300

    HotFix

---
+++ myCourses/JavaScript-&-Node.JS/Node.JS/React.JS/2/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import CommentsList from "./components/CommentsList";
+import "./components/CommentsListStyle.css";
+
+function App() {
+  return <CommentsList />;
+}
+
+export default App;
